6aa11ae7d248969fc41468f0da20ad360b8b14ec
chewkaycheng <kaycheng.chew@outlook.com>
2025-08-21 01:02:19 +0800

    Fixes

--- ESLAdmin.Infrastructure/Persistence/Identity/IdentityResultMapper.cs
+++ ESLAdmin.Infrastructure/Persistence/Identity/IdentityResultMapper.cs
@@ -1,37 +1,37 @@
-﻿using ErrorOr;
-using ESLAdmin.Common.CustomErrors;
-using FastEndpoints;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.HttpResults;
+﻿//using ErrorOr;
+//using ESLAdmin.Common.CustomErrors;
+//using FastEndpoints;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Http.HttpResults;
 
-namespace ESLAdmin.Infrastructure.Persistence.Identity;
+//namespace ESLAdmin.Infrastructure.Persistence.Identity;
 
-//------------------------------------------------------------------------------
-//
-//                      static class IdentityResultMapper
-//
-//------------------------------------------------------------------------------
-public static class IdentityResultMapper
-{
-  public static Results<Ok<T>, ProblemDetails, InternalServerError> MapToResult<T>(Error error)
-  {
-    var statusCode = error.Code switch
-    {
-      "Identity.UserNotFound" => StatusCodes.Status404NotFound,
-      "Identity.RoleNotFound" => StatusCodes.Status400BadRequest,
-      "Identity.UserAlreadyInRole" => StatusCodes.Status400BadRequest,
-      "Identity.InvalidRoleName" => StatusCodes.Status400BadRequest,
-      "Identity.ConcurrencyError" => StatusCodes.Status409Conflict,
-      _ => StatusCodes.Status500InternalServerError
-    };
+////------------------------------------------------------------------------------
+////
+////                      static class IdentityResultMapper
+////
+////------------------------------------------------------------------------------
+//public static class IdentityResultMapper
+//{
+//  public static Results<Ok<T>, ProblemDetails, InternalServerError> MapToResult<T>(Error error)
+//  {
+//    var statusCode = error.Code switch
+//    {
+//      "Identity.UserNotFound" => StatusCodes.Status404NotFound,
+//      "Identity.RoleNotFound" => StatusCodes.Status400BadRequest,
+//      "Identity.UserAlreadyInRole" => StatusCodes.Status400BadRequest,
+//      "Identity.InvalidRoleName" => StatusCodes.Status400BadRequest,
+//      "Identity.ConcurrencyError" => StatusCodes.Status409Conflict,
+//      _ => StatusCodes.Status500InternalServerError
+//    };
 
-    if (statusCode == StatusCodes.Status500InternalServerError)
-    {
-      return TypedResults.InternalServerError();
-    }
-
-    return new ProblemDetails(
-        ErrorUtils.CreateFailureList(error.Code, error.Description),
-        statusCode);
-  }
-}
+//    if (statusCode == StatusCodes.Status500InternalServerError)
+//    {
+//      return TypedResults.InternalServerError();
+//    }
+    
+//    return new ProblemDetails(
+//        ErrorUtils.CreateFailureList(error.Code, error.Description),
+//        statusCode);
+//  }
+//}
