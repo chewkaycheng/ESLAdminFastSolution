f71f25e0df781b8dd865ef11dc72fa109fccf53b
chewkaycheng <kaycheng.chew@outlook.com>
2025-08-26 19:54:04 +0800

    Fixes

---
+++ ESLAdmin.Features/Countries/UpdateCountry/UpdateCountry.Models.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESLAdmin.Features.Countries.UpdateCountry;
+
+//------------------------------------------------------------------------------
+//
+//                           class UpdateCountryRequest
+//
+//------------------------------------------------------------------------------
+public class UpdateCountryRequest
+{
+  public long CountryId { get; set; }
+  public string CountryName { get; set; } = string.Empty;
+  public string LanguageName {  get; set; } = string.Empty;
+  public long UserCode { get; set; }
+  public string Guid { get; set; } = string.Empty;
+}
+
+//------------------------------------------------------------------------------
+//
+//                           class UpdateCountryResponse
+//
+//------------------------------------------------------------------------------
+public class UpdateCountryResponse
+{
+  public long CountryId { get; set; }
+  public string Guid { get; set; } = string.Empty;
+}
